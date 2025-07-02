d5a0fb448db48c53ab958cbe24d4b5dd241a2e89
Zubayer Ahamed <zubayerahamed.freelancer@gmail.com>
2025-07-02 17:52:20 +0600

    02-07-2025

--- src/main/java/com/zayaanit/todoist/anno/RestApiController.java
+++ src/main/java/com/zayaanit/todoist/anno/RestApiController.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Zubayer Ahamed
  * @since Jun 26, 2025
  */
-@java.lang.annotation.Target(ElementType.TYPE)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @RestController
