bdf82b417d7027577dc2b6ffa3814652040c870f
Zubayer Ahamed <zubayerahamed.freelancer@gmail.com>
2025-08-10 22:22:10 +0600

    10-08-2025

--- src/main/java/com/zayaanit/module/auth/RegisterRequestDto.java
+++ src/main/java/com/zayaanit/module/auth/RegisterRequestDto.java
@@ -1,5 +1,6 @@
 package com.zayaanit.module.auth;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,8 +16,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequestDto {
 
+	@NotBlank(message = "First name required")
 	private String firstName;
 	private String lastName;
+	@NotBlank(message = "Email address required")
 	private String email;
+	@NotBlank(message = "Password required")
 	private String password;
 }
